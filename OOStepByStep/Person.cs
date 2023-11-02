90d502a7da36055d5589854d4a1b22e22ad34cf3
Diana Jing <DJing@slb.com>
2023-11-02 17:46:24 +0800

    feat: Implement the AC1, introduce with name and age.

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Introduce()
+        {
+            string introduction = $"My name is {name}. I am {age} years old.";
+            return introduction;
+        }
+    }
+}
