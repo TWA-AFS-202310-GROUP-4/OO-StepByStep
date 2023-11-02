aea0051632c58c67f3019a44d20f0fe1d86fd713
Xinwen Xiang <XXiang2@slb.com>
2023-11-02 17:43:55 +0800

    feat: finish AC1 that person can introduce name and age, remove useless template code

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string SelfIntroduce()
+        {
+            return IntroduceName() + " " + IntroduceAge();
+        }
+
+        private string IntroduceName()
+        {
+            return $"My name is {name}.";
+        }
+
+        private string IntroduceAge()
+        {
+            return $"I am {age} years old.";
+        }
+    }
+}
---
+++ OOStepByStepTest/StepByStepTest.cs
@@ -0,0 +1,21 @@
+﻿using OOStepByStep;
+using Xunit;
+
+namespace OOStepByStepTest
+{
+    public class StepByStepTest
+    {
+        [Fact]
+        public void Should_return_correct_name_and_age_when_people_Selfintroduce_given_name_and_age()
+        {
+            // Given
+            var person = new Person("Tom", 20);
+            var expectedMes = "My name is Tom. I am 20 years old.";
+            //when
+            var res = person.SelfIntroduce();
+
+            //then
+            Assert.Equal(expectedMes, res);
+        }
+    }
+}
