b7f386535378d4aee09ea2137298611d70aaee41
Ming Wang <MWang35@slb.com>
2023-11-02 22:22:07 +0800

    feat: create Class class that can introduce with classnum.

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 namespace OOStepByStepTest
 {
+    using System.Xml.Linq;
     using OOStepByStep;
     using Xunit;
 
@@ -30,6 +31,49 @@ namespace OOStepByStepTest
             var p = new Student(name, age);
             var ret = p.Introduce();
             Assert.Equal($"My name is {name}. I am {age} years old. I am a student.", ret);
+        }
+
+        [Theory]
+        [InlineData("Tom", 12, 1)]
+        public void Should_return_introduce_name_and_age_and_student_with_class(string name, int age, int classnum)
+        {
+            var classroom = new Class(classnum);
+            var p = new Student(name, age);
+            classroom.Students.Add(p);
+            var ret = classroom.Introduce(p);
+            Assert.Equal($"My name is {name}. I am {age} years old. I am a student of class {classnum}.", ret);
+        }
+
+        [Theory]
+        [InlineData("Tom", 12, 1)]
+        public void Should_return_introduce_name_and_age_and_notsame_student_with_class(string name, int age, int classnum)
+        {
+            var classroom = new Class(classnum);
+            var p = new Student(name, age);
+            classroom.Students.Add(p);
+            var q = new Student("hh", 13);
+            var ret = classroom.Introduce(q);
+            Assert.Equal(string.Empty, ret);
+        }
+
+        [Theory]
+        [InlineData("Tom", 30, 1)]
+        public void Should_return_introduce_name_and_age_and_teacher_with_class(string name, int age, int classnum)
+        {
+            var classroom = new Class(classnum);
+            classroom.Teacher = new Teacher(name, age);
+            var ret = classroom.Introduce(classroom.Teacher);
+            Assert.Equal($"My name is {name}. I am {age} years old. I am a teacher of class {classnum}.", ret);
+        }
+
+        [Theory]
+        [InlineData("Tom", 30, 1)]
+        public void Should_return_introduce_name_and_age_and_notsame_teacher_with_class(string name, int age, int classnum)
+        {
+            var classroom = new Class(classnum);
+            var teacher = new Teacher(name, age);
+            var ret = classroom.Introduce(teacher);
+            Assert.True(ret == string.Empty);
         }
     }
 }
