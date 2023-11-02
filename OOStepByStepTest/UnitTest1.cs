34be26f0a674266465d64320ddd3a8d028217cd5
Ming Wang <MWang35@slb.com>
2023-11-02 17:22:49 +0800

    feat: create student and teacher class that can introduce itself.

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -13,5 +13,23 @@ namespace OOStepByStepTest
             var ret = p.Introduce();
             Assert.Equal($"My name is {name}. I am {age} years old.", ret);
         }
+
+        [Theory]
+        [InlineData("Tom", 12)]
+        public void Should_return_introduce_name_and_age_and_teacher(string name, int age)
+        {
+            var p = new Teacher(name, age);
+            var ret = p.Introduce();
+            Assert.Equal($"My name is {name}. I am {age} years old. I am a teacher.", ret);
+        }
+
+        [Theory]
+        [InlineData("Tom", 12)]
+        public void Should_return_introduce_name_and_age_and_student(string name, int age)
+        {
+            var p = new Student(name, age);
+            var ret = p.Introduce();
+            Assert.Equal($"My name is {name}. I am {age} years old. I am a student.", ret);
+        }
     }
 }
